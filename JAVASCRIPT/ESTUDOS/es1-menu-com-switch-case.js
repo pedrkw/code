f2c75c98406b589225662db0a0e86770679b8de7
Pedro Jorge <45081159+pedrkw@users.noreply.github.com>
2023-10-10 16:23:03 -0300

    feat: Add 'ESTUDOS' folder and upload the first file

---
+++ JAVASCRIPT/ESTUDOS/es1-menu-com-switch-case.js
@@ -0,0 +1,59 @@
+//Elabore um programa que mostre um menu de cadastro, exclusão, alteração e consulta de clientes
+let clientes = [];
+
+while (true) {
+  console.log("Cadastro de clientes\n0 - Incluir\n1 - Alterar\n2 - Excluir\n3 - Consultar\n4 - Parar")
+  let escolha = parseInt(prompt("Opção: "));
+  switch (escolha) {
+    case 0:
+      let incluir = prompt("Digite o nome do cliente: ");
+      clientes.push(incluir);
+      console.log("Cliente cadastrado.");
+      break;
+    case 1:
+      console.log("Lista de clientes cadastrados: ");
+      for (let i = 0; i < clientes.length; i++) {
+        console.log(`${[i]} - ${clientes[i]}`);
+      }
+
+      let escolhaAlterar = parseInt(prompt("Escolha o cliente que deseja alterar: "));
+      if (typeof escolhaAlterar !== 'number' || isNaN(escolhaAlterar) || escolhaAlterar < 0 || escolhaAlterar >= clientes.length) {
+        console.log("Escolha inválida.");
+        break
+      }
+      let alterar = prompt("Digite o novo nome: ");
+      clientes[escolhaAlterar] = alterar
+      break;
+    case 2:
+      console.log("Lista de clientes cadastrados: ");
+      for (let i = 0; i < clientes.length; i++) {
+        console.log(`${[i]} - ${clientes[i]}`);
+      }
+
+      escolhaAlterar = parseInt(prompt("Escolha o cliente que deseja excluir: "));
+      if (typeof escolhaAlterar !== 'number' || isNaN(escolhaAlterar) || escolhaAlterar < 0 || escolhaAlterar >= clientes.length) {
+        clientes.pop(escolhaAlterar);
+        console.log("Cliente excluído com sucesso.")
+      } else {
+        console.log("Escolha inválida.");
+        break;
+      }
+      break;
+    case 3:
+      console.log("Lista de clientes cadastrados: ");
+      for (let i = 0; i < clientes.length; i++) {
+        console.log(`${[i]} - ${clientes[i]}`);
+      }
+      if (clientes.length == 0) {
+        console.log("Nenhum cliente cadastrado.")
+      }
+      break;
+    default:
+      break;
+  }
+
+  if (escolha === 4) {
+    console.log("Programa interrompido.")
+    break
+  }
+}
