ba290605d88777b7c30df3090bcb7eec5869bc79
Pedro Jorge <45081159+pedrkw@users.noreply.github.com>
2023-10-25 11:50:12 -0300

    Rewrite the '3-producer-consumer' code for improved readability and efficiency.

--- JAVASCRIPT/ESTUDOS/3-produtor-consumidor.js
+++ JAVASCRIPT/ESTUDOS/3-produtor-consumidor.js
@@ -1,22 +1,20 @@
 let BUFFER_SIZE = 5; // Tamanho do buffer
 let buffer = []; // Inicialização do buffer
 
-// Função do produtor
 function produtor(item) {
   if (buffer.length < BUFFER_SIZE) { // Verifica se o buffer não está cheio
     buffer.push(item); // Adiciona o item ao buffer
-    console.log(`Produtor produziu ${item}`);
+    console.log(`Produzido: ${item}`);
     console.log(`Buffer: ${buffer}`);
   } else {
     console.log("Buffer cheio. Produtor aguardando.");
   }
 }
 
-// Função do consumidor
 function consumidor() {
   if (buffer.length > 0) { // Verifica se o buffer não está vazio
     const item = buffer.shift(); // Remove o primeiro item do buffer
-    console.log(`Consumidor consumiu ${item}`);
+    console.log(`Consumido: ${item}`);
     console.log(`Buffer: ${buffer}`);
   } else {
     console.log("Buffer vazio. Consumidor aguardando.");
@@ -28,22 +26,15 @@ function numeroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// // Executar 10 produções e 10 consumos
-// for (let i = 1; i <= 10; i++) {
-//   //   produtor(i);
-//   produtor(numeroAleatorio(1, 1000));
-//   consumidor();
-// }
+const produzir = 5;
+const consumir = 5;
 
-for (let i = 1; i <= 10; i++) {
-  if (i <= BUFFER_SIZE) {
-    produtor(numeroAleatorio(1, 1000));
-  } else {
-    console.log("Produtor tentando produzir mais itens do que o buffer permite.");
-    break
-  }
-  consumidor();
+for (let i = 1; i <= produzir; i++) {
+  produtor(numeroAleatorio(1, 1000));
 }
 
+for (let i = 1; i <= consumir; i++) {
+  consumidor();
+}
 
 console.log("Todas as operações concluídas.");
