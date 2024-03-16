2a1e937f2982e1acb3eb6657772e5d208c6722b9
Pedro Jorge <45081159+pedrkw@users.noreply.github.com>
2024-03-16 19:38:11 -0300

    Update: Simplify circle area calculation

--- DART/LISTA-01/01.dart
+++ DART/LISTA-01/01.dart
@@ -2,10 +2,9 @@
 import 'dart:io' as io;
 
 void main() {
-  double pi = 3.14;
   print("Digite o raio do circulo: ");
   String? input = io.stdin.readLineSync();
   double raio = double.parse(input!);
-  double area = pi * raio * raio;
+  double area = 3.14 * raio * raio;
   print("A área do circulo é: ${area.toStringAsFixed(2)}");
 }
